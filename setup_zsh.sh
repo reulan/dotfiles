70b5053ad40829b3fe18fcb06b2cfdaad5352bd5
Michael Simo <michael@mpmsimo.com>
2015-05-10 13:21:50 -0400

    Update setup_zsh.sh
    Added additional steps for a fresh zsh installation.

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,7 +1,19 @@
 # Install zsh from package manger
+echo "Install zsh package"
+# sudo yum install zsh
+sudo apt-get install zsh -y
 
 # Check verison
+echo "Checking zsh version..."
 zsh --version
 
 # Make ZSH default shell
+echo "Making ZSH the default shell"
 chsh -s $(chsh -l | grep "zsh" -m 1)
+
+echo "Installing Oh My ZSH!"
+cd ~
+wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+
+echo "Current shell is: $SHELL"
+echo "Setup complete."
