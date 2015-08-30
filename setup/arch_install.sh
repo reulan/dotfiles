4f2c8a74200c8a368a88e10950f0e8e792eca191
Michael Simo <os@mpmsimo.com>
2015-08-30 04:57:42 +0000

    Added basic ArchLinux package insall script

---
+++ setup/arch_install.sh
@@ -0,0 +1,8 @@
+# For the next Arch Linux install, get these essential packages.
+pacman -S openssh
+
+# Packages needed so I can look at the screen comfortably, a display driver may be needed as well.
+pacman -S xorg-server xorg-xinit xmonad xmonad-config xmobar 
+
+# Packages for tools and applications
+pacman -S git vim
