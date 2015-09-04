98996943fa6059d59fcbc3727a6f06b65c09c6c4
Michael Simo <mpmsimo@gmail.com>
2015-09-03 23:08:29 -0400

    Update dotfiles_install.sh

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -9,13 +9,13 @@ cp $HOME/dotfiles/.vimrc $HOME
 echo "Moving .zshrc to home directory"
 cp $HOME/dotfiles/.zshrc $HOME
 
-#echo "Moving .tmux.conf to home directory"
-#cp $HOME/dotfiles/.tmux.conf $HOME
-
 echo "Moving .gitconfig to home directory"
 cp $HOME/dotfiles/.gitconfig $HOME
 
 echo "Moving .vim to home directory"
 cp -r $HOME/dotfiles/.vim $HOME
+
+#echo "Moving .tmux.conf to home directory"
+#cp $HOME/dotfiles/.tmux.conf $HOME
 
 echo "Setup complete"
