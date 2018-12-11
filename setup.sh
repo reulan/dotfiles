ad20540e06d2c1338d358930767ac949ba6db79c
Michael Simo <os@mpmsimo.com>
2018-12-11 12:54:30 -0800

    rm i3

--- setup.sh
+++ setup.sh
@@ -47,22 +47,6 @@ install_zsh(){
     echo -e "${purple} ZSH version is $(zsh --version). ${rnl}" 
 }
 
-# Window Manager
-install_i3(){
-    ### Experimental
-    echo -e "${blue}Installing i3${rnl}\n"
-
-    # i3 WM repos
-    #echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
-    #sudo apt-get update
-    # TODO: Unsecure!!! Fix this later.
-    #sudo apt-get install i3 -y
-    #sudo apt-get --allow-unauthenticated install sur5r-keyring
-
-    # feh
-    #sudo apt-get install feh -y
-}
-
 # Vim
 install_vim(){
     echo -e "${blue}Installing vim${rnl}\n"
@@ -110,7 +94,6 @@ install_baseos(){
             then
                 install_ubuntu
             fi
-    #install_i3
     elif [[ $OSTYPE == "darwin"* ]]
     then
     install_mac
