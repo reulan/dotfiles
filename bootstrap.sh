95213b9b17c05f6b86e9c78aad2daca64c95c449
Michael <mpmsimo@gmail.com>
2020-08-07 12:06:48 -0700

    macos: idempotent script (#37)
    * create nvim dir
    
    * working for macos with colorize

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,8 @@ KITTY_PATH="${CONFIG_PATH}/kitty"
 NVIM_PATH="${CONFIG_PATH}/nvim"
 
 # Define files that will need to be transferred.
-SHARED_DOTFILES=('.zshrc' '.gitconfig' '.dotenv')
+# nvim note: .vimrc is required for init.vim untill it's fully converted over
+SHARED_DOTFILES=('.zshrc' '.gitconfig' '.dotenv' '.vimrc')
 KITTY_CONFIGS=('kitty.conf' 'colorscheme.conf' 'keybindings.conf')
 MACOS_DOTFILES=('.skhdrc' '.yabairc' 'Brewfile')
 POPOS_DOTFILES=('Brewfile')
@@ -135,6 +136,7 @@ install_kitty(){
 # Text editor (nvim, vim-plug)
 # =========================================
 install_nvim(){
+  mkdir -p ${NVIM_PATH}
   echo "Copying ${SHARED_PATH}/.vim > [${HOME}/.vim]."
   cp -r "${SHARED_PATH}/.vim" ${HOME} || true
 
