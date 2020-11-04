2e047f787f838ecb6757088d7ae1974176b99bc8
Michael Simo <mpmsimo@gmail.com>
2020-11-04 10:19:39 -0800

    arch: add prereq for google cloud sdk

--- bootstrap.sh
+++ bootstrap.sh
@@ -84,12 +84,18 @@ install_popos(){
 
 install_arch(){
   echo -e "${blue}Installing Arch Linux operating system settings.${rnl}\n"
-  sudo pacman -Syuq --noconfirm vim neovim git kitty discord chromium terraform terragrunt
+  sudo pacman -Syuq --noconfirm base-devel vim neovim git kitty discord chromium terraform terragrunt
 
   install_oh_my_zsh
   configure_kitty
   configure_nvim
   link_shared
+
+  mkdir -p $HOME/kit/dotfiles
+  cd $HOME/kit/dotfiles
+
+  git clone https://aur.archlinux.org/google-cloud-sdk.git
+  makepkg -si
 }
 
 # =========================================
